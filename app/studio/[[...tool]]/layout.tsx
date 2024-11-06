629577c6c7b5c8a9338dcf7c802181645089889c
Priyansh <Priyanshyadav1012@gmail.com>
2024-11-07 04:55:41 +0530

    1:18 hr

---
+++ app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata ={
+    title: 'Home',
+    description: 'Welcome to my website',
+}
+
+export default function RootLayout({
+    children,
+}:{children : React.ReactNode;
+    }) {
+        return (
+            <html lang="en">
+                <body>{children}</body>
+            </html>
+        )
+}
